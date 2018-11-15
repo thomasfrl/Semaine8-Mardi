3aee51a0697344e1213e0b9d5d8e2dd8bd5bcdd0
Mathieu Brun-Picard <b00673732@essec.edu>
2018-11-15 14:53:07 +0100

    Category for tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+3.times do
+  my_category = Category.create(title: Faker::Book.genre)
+  3.times do
+    my_task = Task.new(title: Faker::Book.title, deadline: Faker::Date.forward(23))
+    my_task.category = my_category
+    my_task.save
+  end
+end
