de345c1b140f67927234d75499d73b59d0d4f7c8
Mathieu Brun-Picard <b00673732@essec.edu>
2018-11-15 14:58:08 +0100

    Nice image for tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_16_110123) do
+ActiveRecord::Schema.define(version: 2018_11_16_110124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_11_16_110123) do
     t.boolean "status", default: false
     t.datetime "deadline"
     t.bigint "category_id"
+    t.string "image"
     t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,9 @@ require 'faker'
 3.times do
   my_category = Category.create(title: Faker::Book.genre)
   3.times do
-    my_task = Task.new(title: Faker::Book.title, deadline: Faker::Date.forward(23))
+    my_task = Task.new(title: Faker::Book.title,
+                      deadline: Faker::Date.forward(23),
+                      image: Faker::Avatar.image)
     my_task.category = my_category
     my_task.save
   end
